db10b22f133261861615f546541726f5686195a7
Mukesh Kumar <mukesh.beis@gmail.com>
2023-02-06 21:48:01 +0530

    buildSrc & gradle.kts setup

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,2 +1,8 @@
-class AppConfig {
+object AppConfig {
+    const val compileSdk = 32
+    const val minSdk = 21
+    const val versionCode = 1
+    const val targetSdk = 32
+    const val versionName = "1.0.0"
+    const val applicationId = "com.mkr.androidinterviewquestions"
 }
